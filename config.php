a1ac0199cdef57023cf1bb560a048ec9c80a95ce
rmenadclases <rmenadclases@gmail.com>
2024-09-18 16:29:53 +0200

    ELIMINO ARCVHISO FUSION

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/GamePetition/httpdocs/vendor/autoload.php';
-
-$connection = new mysqli("localhost", "root", "", "shnj95");
-if (!$connection) {
-    die('Error de Conexión: ' . mysqli_connect_errno());
-}
-$connection->set_charset("utf8");
